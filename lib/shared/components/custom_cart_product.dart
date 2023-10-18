3d40b2aefd56adebd25f1b7112aee1b9a1acbeab
mohamed3sman <mohamedabdoosman12@gmail.com>
2023-10-18 23:24:28 +0300

    cart view done

---
+++ lib/shared/components/custom_cart_product.dart
@@ -0,0 +1,119 @@
+import 'package:flutter/material.dart';
+
+// ignore: must_be_immutable
+class CustomCartProduct extends StatefulWidget {
+  const CustomCartProduct({
+    super.key,
+    required this.productImage,
+    required this.productName,
+    required this.productUnit,
+    required this.productPrice,
+  });
+
+  final String productImage;
+  final String productName;
+  final String productUnit;
+  final double productPrice;
+
+  @override
+  State<CustomCartProduct> createState() => _CustomCartProductState();
+}
+
+class _CustomCartProductState extends State<CustomCartProduct> {
+  int counter = 1;
+  double? price;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      contentPadding: EdgeInsets.zero,
+      leading: Padding(
+        padding: const EdgeInsets.only(right: 20),
+        child: Image.asset(widget.productImage),
+      ),
+      title: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(
+            widget.productName,
+            style: const TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+          const Icon(Icons.close_rounded),
+        ],
+      ),
+      subtitle: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+        Padding(
+          padding: const EdgeInsets.only(top: 3),
+          child: Text(
+            '${widget.productUnit}, Price',
+            style: const TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+        ),
+        const SizedBox(
+          height: 15,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 18),
+                  child: GestureDetector(
+                    onTap: () {
+                      counter > 1 ? counter-- : counter = 1;
+                      setState(() {});
+                    },
+                    child: const Icon(
+                      Icons.minimize_rounded,
+                      size: 30,
+                      color: Colors.red,
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  width: 20,
+                ),
+                Text(
+                  '$counter',
+                  style: const TextStyle(
+                    fontSize: 22,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+                const SizedBox(
+                  width: 20,
+                ),
+                GestureDetector(
+                  onTap: () {
+                    counter++;
+                    setState(() {});
+                  },
+                  child: const Icon(
+                    Icons.add,
+                    size: 30,
+                    color: Colors.green,
+                  ),
+                ),
+              ],
+            ),
+            Text(
+              '\$${widget.productPrice * counter}',
+              style: const TextStyle(
+                fontSize: 19,
+                fontWeight: FontWeight.w600,
+                color: Colors.black,
+              ),
+            ),
+          ],
+        ),
+      ]),
+    );
+  }
+}
--- lib/views/cart_view/widgets/cart_view_body.dart
+++ lib/views/cart_view/widgets/cart_view_body.dart
@@ -1,12 +1,118 @@
 import 'package:flutter/material.dart';
+import 'package:grocery_app/shared/components/custom_cart_product.dart';
 
-class CartViewBody extends StatelessWidget {
+class CartViewBody extends StatefulWidget {
   const CartViewBody({super.key});
 
   @override
+  State<CartViewBody> createState() => _CartViewBodyState();
+}
+
+class _CartViewBodyState extends State<CartViewBody> {
+  @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Cart'),
+    return const Padding(
+      padding: EdgeInsets.only(top: 50),
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'My Cart',
+                  style: TextStyle(
+                    fontSize: 22,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Divider(
+              color: Colors.grey,
+              endIndent: 0,
+              height: 0.5,
+              indent: 0,
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 25),
+              child: Column(
+                children: [
+                  CustomCartProduct(
+                    productImage: 'assets/images/pepper.png',
+                    productName: 'Bell Pepper Red',
+                    productUnit: '1Kg',
+                    productPrice: 1.99,
+                  ),
+                  SizedBox(
+                    height: 25,
+                  ),
+                  Divider(
+                    color: Colors.grey,
+                    endIndent: 0,
+                    height: 0.5,
+                    indent: 0,
+                  ),
+                  SizedBox(
+                    height: 25,
+                  ),
+                  CustomCartProduct(
+                    productImage: 'assets/images/egg.png',
+                    productName: 'Egg Chicken Red',
+                    productUnit: '4Pcs',
+                    productPrice: 1.99,
+                  ),
+                  SizedBox(
+                    height: 25,
+                  ),
+                  Divider(
+                    color: Colors.grey,
+                    endIndent: 0,
+                    height: 0.5,
+                    indent: 0,
+                  ),
+                  SizedBox(
+                    height: 25,
+                  ),
+                  CustomCartProduct(
+                    productImage: 'assets/images/banana.png',
+                    productName: 'Organic Bananas',
+                    productUnit: '2Kg',
+                    productPrice: 1.99,
+                  ),
+                  SizedBox(
+                    height: 25,
+                  ),
+                  Divider(
+                    color: Colors.grey,
+                    endIndent: 0,
+                    height: 0.5,
+                    indent: 0,
+                  ),
+                  SizedBox(
+                    height: 25,
+                  ),
+                  CustomCartProduct(
+                    productImage: 'assets/images/ginger.png',
+                    productName: 'Ginger',
+                    productUnit: '250g',
+                    productPrice: 1.99,
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
