7ee503efa2182e907dd2b32de8a07123d51f4776
mohamed3sman <mohamedabdoosman12@gmail.com>
2023-10-25 21:01:28 +0300

    order failed bottom sheet done

--- lib/views/cart_view/widgets/cart_view_body.dart
+++ lib/views/cart_view/widgets/cart_view_body.dart
@@ -116,139 +116,7 @@ class _CartViewBodyState extends State<CartViewBody> {
                   DefaultButton(
                     text: 'Go to Chackout',
                     onPressed: () {
-                      showModalBottomSheet(
-                          context: context,
-                          builder: (context) {
-                            return SizedBox(
-                              width: double.infinity,
-                              child: Column(
-                                children: [
-                                  const Padding(
-                                    padding: EdgeInsets.symmetric(
-                                        vertical: 30, horizontal: 25),
-                                    child: Row(
-                                      mainAxisAlignment:
-                                          MainAxisAlignment.spaceBetween,
-                                      children: [
-                                        Text(
-                                          'Checkout',
-                                          style: TextStyle(
-                                            fontSize: 22,
-                                            fontWeight: FontWeight.w700,
-                                          ),
-                                        ),
-                                        Icon(
-                                          Icons.close,
-                                          size: 30,
-                                        ),
-                                      ],
-                                    ),
-                                  ),
-                                  const Divider(
-                                    color: Colors.grey,
-                                    endIndent: 0,
-                                    height: 0.5,
-                                    indent: 0,
-                                  ),
-                                  const SizedBox(
-                                    height: 25,
-                                  ),
-                                  Padding(
-                                    padding: const EdgeInsets.symmetric(
-                                        horizontal: 25),
-                                    child: Column(
-                                      children: [
-                                        const CheckoutBottomSheetContainer(
-                                          title: 'Delivery',
-                                          subTitle: 'Select Method',
-                                        ),
-                                        const Padding(
-                                          padding: EdgeInsets.symmetric(
-                                              vertical: 20),
-                                          child: Divider(
-                                            color: Colors.grey,
-                                            endIndent: 0,
-                                            height: 0.5,
-                                            indent: 0,
-                                          ),
-                                        ),
-                                        Row(
-                                          mainAxisAlignment:
-                                              MainAxisAlignment.spaceBetween,
-                                          children: [
-                                            Text(
-                                              'Payment',
-                                              style: TextStyle(
-                                                  fontSize: 20,
-                                                  fontWeight: FontWeight.w700,
-                                                  color: Colors.black
-                                                      .withOpacity(0.6)),
-                                            ),
-                                            const Row(
-                                              children: [
-                                                Icon(
-                                                  Icons.payment_rounded,
-                                                  size: 30,
-                                                  color: primaryColor,
-                                                ),
-                                                SizedBox(
-                                                  width: 5,
-                                                ),
-                                                Icon(
-                                                  Icons
-                                                      .arrow_forward_ios_rounded,
-                                                ),
-                                              ],
-                                            ),
-                                          ],
-                                        ),
-                                        const Padding(
-                                          padding: EdgeInsets.symmetric(
-                                              vertical: 20),
-                                          child: Divider(
-                                            color: Colors.grey,
-                                            endIndent: 0,
-                                            height: 0.5,
-                                            indent: 0,
-                                          ),
-                                        ),
-                                        const CheckoutBottomSheetContainer(
-                                          title: 'Promo Code',
-                                          subTitle: 'Pick discount',
-                                        ),
-                                        const Padding(
-                                          padding: EdgeInsets.symmetric(
-                                              vertical: 20),
-                                          child: Divider(
-                                            color: Colors.grey,
-                                            endIndent: 0,
-                                            height: 0.5,
-                                            indent: 0,
-                                          ),
-                                        ),
-                                        const CheckoutBottomSheetContainer(
-                                          title: 'Total Cost',
-                                          subTitle: '\$13.97',
-                                        ),
-                                        const SizedBox(
-                                          height: 40,
-                                        ),
-                                        DefaultButton(
-                                            text: 'Place Order',
-                                            onPressed: () {
-                                              Navigator.push(context,
-                                                  MaterialPageRoute(
-                                                      builder: (context) {
-                                                return const PaidSuccessView();
-                                              }));
-                                            })
-                                      ],
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            );
-                          });
+                      PaidFailedBottomSheet(context);
                     },
                   ),
                   const SizedBox(
@@ -262,4 +130,208 @@ class _CartViewBodyState extends State<CartViewBody> {
       ),
     );
   }
+
+  Future<dynamic> PaidSuccessfullyBottomSheet(BuildContext context) {
+    return showModalBottomSheet(
+        context: context,
+        builder: (context) {
+          return SizedBox(
+            width: double.infinity,
+            child: Column(
+              children: [
+                const Padding(
+                  padding: EdgeInsets.symmetric(vertical: 30, horizontal: 25),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'Checkout',
+                        style: TextStyle(
+                          fontSize: 22,
+                          fontWeight: FontWeight.w700,
+                        ),
+                      ),
+                      Icon(
+                        Icons.close,
+                        size: 30,
+                      ),
+                    ],
+                  ),
+                ),
+                const Divider(
+                  color: Colors.grey,
+                  endIndent: 0,
+                  height: 0.5,
+                  indent: 0,
+                ),
+                const SizedBox(
+                  height: 25,
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25),
+                  child: Column(
+                    children: [
+                      const CheckoutBottomSheetContainer(
+                        title: 'Delivery',
+                        subTitle: 'Select Method',
+                      ),
+                      const Padding(
+                        padding: EdgeInsets.symmetric(vertical: 20),
+                        child: Divider(
+                          color: Colors.grey,
+                          endIndent: 0,
+                          height: 0.5,
+                          indent: 0,
+                        ),
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            'Payment',
+                            style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.w700,
+                                color: Colors.black.withOpacity(0.6)),
+                          ),
+                          const Row(
+                            children: [
+                              Icon(
+                                Icons.payment_rounded,
+                                size: 30,
+                                color: primaryColor,
+                              ),
+                              SizedBox(
+                                width: 5,
+                              ),
+                              Icon(
+                                Icons.arrow_forward_ios_rounded,
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                      const Padding(
+                        padding: EdgeInsets.symmetric(vertical: 20),
+                        child: Divider(
+                          color: Colors.grey,
+                          endIndent: 0,
+                          height: 0.5,
+                          indent: 0,
+                        ),
+                      ),
+                      const CheckoutBottomSheetContainer(
+                        title: 'Promo Code',
+                        subTitle: 'Pick discount',
+                      ),
+                      const Padding(
+                        padding: EdgeInsets.symmetric(vertical: 20),
+                        child: Divider(
+                          color: Colors.grey,
+                          endIndent: 0,
+                          height: 0.5,
+                          indent: 0,
+                        ),
+                      ),
+                      const CheckoutBottomSheetContainer(
+                        title: 'Total Cost',
+                        subTitle: '\$13.97',
+                      ),
+                      const SizedBox(
+                        height: 40,
+                      ),
+                      DefaultButton(
+                          text: 'Place Order',
+                          onPressed: () {
+                            Navigator.push(context,
+                                MaterialPageRoute(builder: (context) {
+                              return const PaidSuccessView();
+                            }));
+                          })
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          );
+        });
+  }
+}
+
+Future<dynamic> PaidFailedBottomSheet(BuildContext context) {
+  return showModalBottomSheet(
+      context: context,
+      isScrollControlled: true,
+      builder: (context) {
+        return Container(
+          height: MediaQuery.of(context).size.height * 0.75,
+          width: double.infinity,
+          padding: const EdgeInsets.all(25),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(30),
+          ),
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  GestureDetector(
+                    onTap: () {},
+                    child: const Icon(
+                      Icons.close,
+                      size: 30,
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 25,
+              ),
+              Center(child: Image.asset('assets/images/order-failed.png')),
+              const SizedBox(
+                height: 40,
+              ),
+              SizedBox(
+                width: MediaQuery.of(context).size.width * 0.8,
+                child: const Text(
+                  textAlign: TextAlign.center,
+                  'Oops! Order Failed',
+                  style: TextStyle(
+                    fontSize: 25,
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 5,
+              ),
+              SizedBox(
+                width: MediaQuery.of(context).size.width * 0.75,
+                child: const Text(
+                  textAlign: TextAlign.center,
+                  'Something Went Wrong',
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w700,
+                    color: Colors.grey,
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(
+                    top: 60, bottom: 30, left: 25, right: 25),
+                child: DefaultButton(text: 'Track Order', onPressed: () {}),
+              ),
+              const Text(
+                'Back to home',
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
+            ],
+          ),
+        );
+      });
 }
